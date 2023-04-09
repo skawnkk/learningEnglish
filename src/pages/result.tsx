906ddf92b42fade096b7e19ffcafac2626ce5f31
김남주 <namju.kim@tictoccroc.com>
2023-04-09 17:25:30 +0900

    기본셋팅

---
+++ src/pages/result.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Result() {
+  return <div>테스트결과</div>
+}
+
+export default Result
---
+++ src/pages/test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Test() {
+  return (
+    <div>테스트페이지</div>
+  );
+}
+
+export default Test;
