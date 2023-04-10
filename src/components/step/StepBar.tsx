c565871e9b21a38dab0c8351bc9c8443d8cd3e2e
김남주 <skawn11114@gmail.com>
2023-04-10 20:26:00 +0900

    [#2] 테스트페이지 - StepBar 컴포넌트 생성

---
+++ src/components/step/StepBar.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import styles from './StepBar.module.css'
+import classNames from 'classnames'
+import {Answer, MyTestState} from "../../utils/quiz";
+
+export enum AnswerState {
+  CORRECT,
+  WRONG,
+  TRYING,
+  TODO,
+}
+
+interface Step {
+  state: AnswerState
+  style: React.CSSProperties
+}
+
+const Step = ({state, style}: Step) => {
+  const getAnswerStyle = (state: AnswerState) => {
+    let style
+    switch (state) {
+      case AnswerState.CORRECT:
+        style = styles.correct
+        break
+      case AnswerState.WRONG:
+        style = styles.wrong
+        break
+      case AnswerState.TRYING:
+        style = styles.trying
+        break
+      default:
+        style = styles.todo
+        break
+    }
+
+    return style
+  }
+
+  return <div className={classNames(getAnswerStyle(state), styles.step)} style={style} />
+}
+
+interface StepBar {
+  stepList: Answer[]
+  current: number
+}
+
+function StepBar({stepList, current}: StepBar) {
+
+  return (
+    <>
+      <div className={classNames(styles.stepBar, 'flex justify-around')}>
+        {stepList.map((step, idx) => (
+          <Step key={idx} state={step.state} style={{width: `calc(90%/${stepList.length})`}} />
+        ))}
+      </div>
+      <div className={styles.progress}>
+        {current+1}/{stepList.length}
+      </div>
+    </>
+  )
+}
+
+export default StepBar
