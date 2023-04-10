99fb64cb5d1e4f2659d025c265398fe81ea74241
김남주 <skawn11114@gmail.com>
2023-04-10 22:53:42 +0900

    atomic modal 생성 및 스타일

---
+++ src/atomics/modal/Modal.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './modal.module.css'
+import classNames from 'classnames'
+
+interface ModalProp {
+  show: boolean
+  children: React.ReactElement
+}
+
+function Modal({condition, children}: ModalProp) {
+
+  return (
+    <div className={classNames(styles.modal, styles.fullHeight, condition ? styles.show : styles.none)}>
+      <div className={classNames(styles.modalContents, styles.fullHeight)}>{children}</div>
+    </div>
+  )
+}
+
+export default Modal
