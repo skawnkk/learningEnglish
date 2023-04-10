73d011a73cc30cd70d1c81eab0393b8233bc5da0
김남주 <skawn11114@gmail.com>
2023-04-10 19:56:04 +0900

    상태관리라이브러리 리코일 설치

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import '@/styles/globals.css'
 import type {AppProps} from 'next/app'
+import {RecoilRoot} from "recoil";
 
 export default function App({Component, pageProps}:AppProps) {
  return (
-   <div className={'layout'}>
-    <Component {...pageProps} />
-   </div>
+   <RecoilRoot>
+    <div className={'layout'}>
+      <Component {...pageProps} />
+    </div>
+   </RecoilRoot>
  )
 }
